49aaf1dd0f8dc6d0e061156253b86d4634c02bf1
Mikkel Malmberg <mikkel@brnbw.com>
2017-12-04 11:18:48 +0100

    Keep it to 6 dumps on index

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,41 @@
 import withSubs from "../components/withSubs";
 import Link from "next/link";
 import Layout, { A, TitleBar, BackButton } from "../components/Layout";
+import take from "lodash/take";
 import map from "lodash/map";
 import reverse from "lodash/reverse";
 import FBImage from "../components/FBImage";
 
-const Index = ({ dumps }) =>
+const Index = ({ dumps }) => (
   <Layout>
     <TitleBar>mootmoart</TitleBar>
     <div className="pa3 flex-ns flex-wrap nl2 nr2 mh1">
-      {map(reverse(Object.keys(dumps)), key => {
-        const dump = dumps[key];
-        return (
-          <div key={key} className="ph2 mb3 w-50-ns ">
-            <div className="bg-white shadow-1 br2 overflow-hidden tc flex-none">
-              <div className="w-100 h5 dib overflow-hidden">
-                <Link>
-                  <a href={`/dumps/${key}`}>
-                    <FBImage
-                      fbRef={`gs://${dump.bucket}/${dump.name}`}
-                      className="w-100 db"
-                    />
-                  </a>
-                </Link>
-              </div>
-              <div className="pa1 f6 b">
-                {dump.timeCreated.split("T")[0]}
+      {take(
+        map(reverse(Object.keys(dumps)), key => {
+          const dump = dumps[key];
+          return (
+            <div key={key} className="ph2 mb3 w-50-ns ">
+              <div className="bg-white shadow-1 br2 overflow-hidden tc flex-none">
+                <div className="w-100 h5 dib overflow-hidden">
+                  <Link>
+                    <a href={`/dumps/${key}`}>
+                      <FBImage
+                        fbRef={`gs://${dump.bucket}/${dump.name}`}
+                        className="w-100 db"
+                      />
+                    </a>
+                  </Link>
+                </div>
+                <div className="pa1 f6 b">{dump.timeCreated.split("T")[0]}</div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        }),
+        6
+      )}
     </div>
-  </Layout>;
+  </Layout>
+);
 
 export default withSubs((props, db) => ({
   dumps: db.ref("data/dumps")
